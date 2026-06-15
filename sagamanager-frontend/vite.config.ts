e49abb743077bc55c6c4b586c3dbc3f4356cbdcd
Anton Christensen <anton.l.s.christensen@gmail.com>
2026-06-15 09:45:37 +0200

    first Start

---
+++ sagamanager-frontend/vite.config.ts
@@ -0,0 +1,31 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import analog from '@analogjs/platform';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  build: {
+    target: ['es2020'],
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
+  plugins: [
+    analog({
+      nitro: {
+        preset: "cloudflare_module",
+        compatibilityDate: "2026-06-15"
+      }
+    }),
+    tailwindcss()
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-setup.ts'],
+    include: ['**/*.spec.ts'],
+    reporters: ['default'],
+  },
+}));
